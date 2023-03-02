02f1856c701cba2a9ee2c448eaf4c5f3afae6077
Peera Aronrat <peelaxv@gmail.com>
2023-03-02 12:01:46 +0700

    write readme

---
+++ main.py
@@ -0,0 +1,12 @@
+# Takes a set of sequences from a genome as input
+# Outputs set of short substrings that seem to occur quite often
+
+
+# 1.read sequences from .fasta file and store in array
+
+
+# 2.A set of  t DNA sequences
+# •Each sequence has  n  nucleotides s= (s1, s2, s3, ..., st)
+sequences = []
+# 3.An array contains set of starting positions si in each DNA sequence
+startPositon = []
